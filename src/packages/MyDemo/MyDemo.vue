038c5da302525cd436abe16e8e38c26f910ae30b
lrz <1414556676@qq.com>
2020-06-04 17:03:26 +0800

    init

---
+++ src/packages/MyDemo/MyDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo-container">
+      demo component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyDemo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.demo-container{
+  background: rgb(110, 203, 247)
+}
+</style>
